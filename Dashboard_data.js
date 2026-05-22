1f7af45f9856f434c49a45bc62f1bf1183fb0065
Aziz El Hajir <aelhajir@gmail.com>
2026-05-22 19:28:21 +0200

    fix: index.html complet, fermeture balises et correctif if(hCtx)

--- Dashboard_data.js
+++ Dashboard_data.js
@@ -1,12 +1,12 @@
 // Dashboard_data.js -- genere automatiquement par Dashboard_refresh.py v2
-// 22/05/2026 14:28 -- MEMNM-2026
+// 22/05/2026 17:58 -- MEMNM-2026
 // NE PAS EDITER MANUELLEMENT -- relancer Dashboard_refresh.py
 
 const D = {
   "meta": {
     "version": "v2",
     "generated": "2026-05-22",
-    "generated_full": "22/05/2026 14:28",
+    "generated_full": "22/05/2026 17:58",
     "source": "Dashboard_refresh.py v2",
     "poly_ia": {
       "convention_nommage": "MEMNM2026_[MODULE]_[OBJET]_V[x.y]_[DATE].ext",
@@ -540,18 +540,18 @@ const D = {
     "01_Drafts_Chapitres": 21,
     "02_Livrables_DEC030": 6,
     "03_Pilotage_Inventaire": 20,
-    "04_Bibliographie": 3,
+    "04_Bibliographie": 4,
     "05_Diagnostic_Audit": 2,
     "06_Enquete_Pilote": 29,
     "07_Annexes_1-16": 27,
     "08_SSOT_V7.2": 61,
-    "09_Scripts": 31,
+    "09_Scripts": 33,
     "10_Social_Listening": 103,
     "_ARCHIVE": 79,
-    "__total__": 397,
+    "__total__": 400,
     "__date__": "2026-05-22"
   },
-  "delta_fichiers": 31,
+  "delta_fichiers": 34,
   "chapitres": [],
   "heures": [
     {
