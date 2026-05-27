d6abb796ffb0883c6e86144c2c91562c929545b9
Aziz El Hajir <aelhajir@gmail.com>
2026-05-27 10:53:02 +0200

    refresh: 2026-05-27 10:53 (auto)

--- Dashboard_data.js
+++ Dashboard_data.js
@@ -1,12 +1,12 @@
 // Dashboard_data.js -- genere automatiquement par Dashboard_refresh.py v2
-// 27/05/2026 10:45 -- MEMNM-2026
+// 27/05/2026 10:53 -- MEMNM-2026
 // NE PAS EDITER MANUELLEMENT -- relancer Dashboard_refresh.py
 
 const D = {
   "meta": {
     "version": "v2",
     "generated": "2026-05-27",
-    "generated_full": "27/05/2026 10:45",
+    "generated_full": "27/05/2026 10:53",
     "source": "Dashboard_refresh.py v2",
     "poly_ia": {
       "convention_nommage": "MEMNM2026_[MODULE]_[OBJET]_V[x.y]_[DATE].ext",
