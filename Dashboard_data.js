cb7bc10c821b17f9e7fbfa3cc7f02c471b31c862
Aziz El Hajir <aelhajir@gmail.com>
2026-05-27 11:59:27 +0200

    refresh: 2026-05-27 11:59 (auto)

--- Dashboard_data.js
+++ Dashboard_data.js
@@ -1,12 +1,12 @@
 // Dashboard_data.js -- genere automatiquement par Dashboard_refresh.py v2
-// 27/05/2026 11:44 -- MEMNM-2026
+// 27/05/2026 11:59 -- MEMNM-2026
 // NE PAS EDITER MANUELLEMENT -- relancer Dashboard_refresh.py
 
 const D = {
   "meta": {
     "version": "v2",
     "generated": "2026-05-27",
-    "generated_full": "27/05/2026 11:44",
+    "generated_full": "27/05/2026 11:59",
     "source": "Dashboard_refresh.py v2",
     "poly_ia": {
       "convention_nommage": "MEMNM2026_[MODULE]_[OBJET]_V[x.y]_[DATE].ext",
@@ -562,13 +562,13 @@ const D = {
     "06_Enquete_Pilote": 29,
     "07_Annexes_1-16": 27,
     "08_SSOT_V7.2": 61,
-    "09_Scripts": 41,
+    "09_Scripts": 39,
     "10_Social_Listening": 103,
     "_ARCHIVE": 79,
-    "__total__": 417,
+    "__total__": 415,
     "__date__": "2026-05-27"
   },
-  "delta_fichiers": 51,
+  "delta_fichiers": 49,
   "chapitres": [],
   "heures": [
     {
